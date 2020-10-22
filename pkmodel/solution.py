987bb616a67babc9dd0eb5102bc054d574c72812
jkrol8 <julia.krol@linacre.ox.ac.uk>
2020-10-22 10:59:16 +0100

    Add draft of the solution class

--- pkmodel/solution.py
+++ pkmodel/solution.py
@@ -1,7 +1,8 @@
 #
 # Solution class
 #
-
+import numpy as np
+import scipy.integrate
 class Solution:
     """A Pharmokinetic (PK) model solution
 
@@ -12,6 +13,28 @@ class Solution:
         an example paramter
 
     """
-    def __init__(self, value=44):
-        self.value = value
+    def __init__(self, list_of_models, t_eval, y0 = [0,0]):
+        self.models = models
+        self.y0 = np.array(y0)
+        self.t_eval = t_eval
+    
+    def _get_arguments(self, model):
+        return model.args #function/property of model from model class?
+    
+    def _integrate(self, model):
+        sol = scipy.integrate.solve_ivp(fun =  lambda t, y: model.rhs(), #function for getting rhs?
+         t_span = [self.t_eval[0], self.t_eval[-1]],
+         y0 = self.y0, t_eval = self.t_eval
+        )
+        return sol
+
+    def analyse_models(self):
+        for model in self.models:
+            args = self.get_arguments(model)
+            sol = self._integrate(model)
+        return sol
+
+
+
+
 
