2c73d4ec314ed7b2704aa8277713b0bab329b737
Emmanuelle BOurigault <emmanuelle.bourigault@dtc.ox.ac.uk>
2020-10-23 18:34:27 +0100

    Unittests for the Protocol class

--- pkmodel/tests/test_protocol.py
+++ pkmodel/tests/test_protocol.py
@@ -1,15 +1,44 @@
 import unittest
-import pkmodel as pk
+import numpy as np
+import numpy.testing as npt
+import pytest
+import Dose
 
+"""
+Testing of the Protocol class from the Dose.py file 
+
+"""
 
 class ProtocolTest(unittest.TestCase):
     """
-    Tests the :class:`Protocol` class.
+    Tests the Protocol class.
     """
-    def test_create(self):
+    def test_steady_dose(self):
         """
-        Tests Protocol creation.
+        Unittests of the steady dose function
         """
-        model = pk.Protocol()
-        self.assertEqual(model.value, 43)
+        protocol = Dose.Protocol()
+        self.assertEqual(protocol.steady_dose().any(), 43)
+
+
+"""
+Multiple unittests of the steady dose function
+"""
+@pytest.mark.parametrize("test, expected", [(43, 43), (-20, 43)])
+def tests_steady_dose(test,expected):
+    protocol = Dose.Protocol()
+    npt.assert_array_equal(protocol.steady_dose(), expected)
+
+
+class ProtocolInput(object):
+    
+     def test_instantaneous_dose_string(self):
+        """
+        Test for TypeError when passing strings in the instantaneous dose function
+        """
+        
+        protocol = Dose.Protocol()
+        with pytest.raises(TypeError):
+             protocol.instantaneous_dose('hello world')
+
 
